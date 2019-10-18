bd1e5ae71704a63c13572e6f5ef01835573b3ec0
pjtuxe <zsolt.fekete@antavo.com>
2019-10-18 14:22:30 +0200

    BM: Installation Status menu

---
+++ cartridges/bm_antavo/cartridge/controllers/AntavoAdminInstall.js
@@ -0,0 +1,66 @@
+/**
+ * @module bm_antavo/controllers
+ */
+
+"use strict"
+
+var CustomObjectMgr = require("dw/object/CustomObjectMgr");
+var Config = require("~/cartridge/scripts/utils/Config");
+var URLUtils = require('dw/web/URLUtils');
+var ServiceRegistry = require("dw/svc/LocalServiceRegistry");
+var Template = require("~/cartridge/scripts/utils/Template");
+
+/**
+ * @param {String} name
+ * @returns {Boolean}
+ */
+function isCustomObjectDefined(name) {
+    try {
+        return !!CustomObjectMgr.describe(name);
+    } catch (e) {
+        return false;
+    }
+}
+
+/**
+ * @returns {Boolean}
+ */
+function isHttpServiceCreated() {
+    try {
+        return !!ServiceRegistry.createService("antavo.http", {});
+    } catch (e) {
+        return false;
+    }
+}
+
+/**
+ * @returns {Boolean}
+ */
+function isHttpServiceConfigured() {
+    try {
+        var service = ServiceRegistry.createService("antavo.http", {});
+        var credential = service.getConfiguration().getCredential();
+        return credential.getUser() && credential.getPassword() && credential.getURL();
+    } catch (e) {
+        return false;
+    }
+}
+
+/**
+ * 
+ */
+function show() {
+    Template.render("antavobm/install/show", { 
+        ContinueURL: URLUtils.https("AntavoAdminSocialShare-ProcessSettings"),
+        activeAntavoPage: 'install',
+        title: 'Installation Status',
+        config: Config.get(),
+        isHttpServiceCreated: isHttpServiceCreated(),
+        isHttpServiceConfigured: isHttpServiceConfigured(),
+        isConfigObjectDefined: isCustomObjectDefined("AntavoConfig"),
+        isQueueObjectDefined: isCustomObjectDefined("AntavoRequestQueue"),
+    });
+}
+
+exports.Show = show;
+exports.Show.public = true;
